0f81da3de7a1c1e8ab2e7fbfd271ff16f5afe0c4
李伟 <liwei@liweideiMac.local>
2018-08-15 13:24:07 +0800

    update

---
+++ websocketpp_demo/websocketpp_demo/echo.h
@@ -0,0 +1,18 @@
+//
+//  echo.h
+//  websocketpp_demo
+//
+//  Created by 李伟 on 2018/8/15.
+//  Copyright © 2018年 李伟. All rights reserved.
+//
+
+#ifndef echo_h
+#define echo_h
+
+
+int echo_server_main(int argc, const char* argv[]);
+
+int echo_client_main(int argc, const char* argv[]);
+
+    
+#endif /* echo_h */
--- websocketpp_demo/websocketpp_demo/main.cpp
+++ websocketpp_demo/websocketpp_demo/main.cpp
@@ -6,10 +6,20 @@
 //  Copyright © 2018年 李伟. All rights reserved.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
+#include "echo.h"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (strcmp(argv[1], "-s") == 0) {
+        echo_server_main(argc, argv);
+    } else if (strcmp(argv[1], "-c") == 0) {
+        echo_client_main(argc, argv);
+    } else {
+        
+    }
+    
     return 0;
 }
